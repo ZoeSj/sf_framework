51a5a29f4f7e5f95351bbeb22afc87933fe003fc
shengjia <shengj@anchnet.com>
2019-03-07 21:42:35 +0800

    描述需要的和依赖的对象

--- web/front.php
+++ web/front.php
@@ -8,57 +8,11 @@
  */
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 $routes = include __DIR__ . '/../src/app.php';
 
-$context = new Routing\RequestContext();
+$framework = new \Simplex\Framework($routes);
+$framework->handle($request)->send();
 
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$dispatcher = new EventDispatcher();
-$listener = new HttpKernel\EventListener\ExceptionListener(
-    'Calendar\Controller\ErrorController::exception'
-);
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-
-$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
-
-$response = $framework->handle($request);
-$response->send();
-
-function render_template($request)
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    /** @var TYPE_NAME $_route */
-    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
-
-$routes = include __DIR__ . '/../src/app.php';
-$request = Request::createFromGlobals();
-
-$context = new  Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
-$response = $framework->handle($request);
-
-$response->send();
