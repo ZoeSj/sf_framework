6a38e6c31ff4d7b6370119bc4212917c0b1ea741
shengjia <shengj@anchnet.com>
2019-03-07 10:07:11 +0800

    获取request的信息

--- index.php
+++ index.php
@@ -12,6 +12,27 @@ use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();//根据当前PHP的全局变量生成一个Request对象
 
+$request->getPathInfo();//请求的URI
+
+//分别得到GET参数或者POST参数
+$request->query->get('foo');//GET
+$request->request->get('bar', '如果没有bar的默认值');//POST
+
+//得到服务器变量
+$request->server->get('HTTP_HOST');
+
+//得到上传文件变量
+$request->files->get('foo');
+
+//得到cookie的值
+$request->cookies->get('PHPSESSID');
+
+//得到http请求头信息
+$request->headers->get('host');
+$request->headers->get('content_type');
+
+$request->getMethod();
+$request->getLanguages();
 $input = $request->get('name', 'World');
 
 $response = new Response(sprintf(
